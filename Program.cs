ac034978d92cf5a7fa71a03ae00702ef8b76d852
prasad <prasadanduri13gmail.com>
2022-03-30 10:25:12 +0530

    workshop commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace WorkShopPro
+{
+    public class Program
+    {
+        static int higestPowerOf2(int n)
+        {
+            int P = 0;
+            for (int i = n; i >= 1; i--)
+            {
+                if ((i & (i - 1) == 0)
+
+               {
+                    P = i;
+                    break;
+                }
+            }
+            return p;
+
+        }
+        static public void Main()
+        {
+            int n = 12;
+            Console.WriteLine(higestPowerOf2(n))
+        }
+    }
+
+
